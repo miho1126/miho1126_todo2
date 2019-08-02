1d4e06d23c3955d8689128a112f3cb98daa817c5
miho1126 <iyq2loveeveryone@gmail.com>
2019-08-02 16:47:24 +0900

    ボタンを関数へ移行

--- app.js
+++ app.js
@@ -11,15 +11,19 @@ if (localStorage.getItem('task')){
 // クリックイベント
 document.getElementById('add').addEventListener('click',
   function(){
+    // 預ける
     // task代入
     const task = document.getElementById('task');
-    // 預ける
+
     // タスクの列を配列dataに追加
     data.push(task.value);
+
     // タスク追加
     creatDOM(task.value);
+
     // dataをローカルストレージに保存
     dataUpdated();
+
     // taskのvalueを初期化
     task.value = "";
   }
@@ -35,25 +39,31 @@ for (const value of data) {
 function creatDOM( value ){
   // liタグを作成
   let list = document.createElement('li');
+
   // リストタグないにvalueを格納
   list.textContent = value;
 
+  // ボタンタグ追加
   let button = document.createElement('button');
   button.textContent = '削除';
   list.appendChild(button);
 
-  button.addEventListener('click', function(){
-    //削除ボタンを消すのがremove
-    this.parentNode.remove();
-    // 削除の前にあるaaaaaaを消すためにその文字を見つける。data.indexOf(this.parentNode.textContent);を下のカッコ内に入れる。
-    // データをspliceで配列を削除する。項目の中から一致するものを探し出す方法。数字のマイナスは後ろから、マイナスなければ前から２個目をとる。
-    data.splice(data.indexOf(this.parentNode.textContent.slice( 0, -2)),1);
-    // データを再度保存
-    localStorage.setItem('task',JSON.stringify(data));
-  })
+// 削除ボタン
+button.addEventListener('click',remove)
 
   // listの子要素として追加
   document.getElementById('list').appendChild(list);
+}
+
+// 削除ボタん
+function remove(){
+  //削除ボタンを消すのがremove
+  this.parentNode.remove();
+  // 削除の前にあるaaaaaaを消すためにその文字を見つける。data.indexOf(this.parentNode.textContent);を下のカッコ内に入れる。
+  // データをspliceで配列を削除する。項目の中から一致するものを探し出す方法。数字のマイナスは後ろから、マイナスなければ前から２個目をとる。
+  data.splice(data.indexOf(this.parentNode.textContent.slice( 0, -2)),1);
+  // データを再度保存
+  dataUpdated();
 }
 
 function dataUpdated(){
